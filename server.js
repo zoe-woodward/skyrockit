a6b15d299e7c4c67a4a489ed370bfcdb6cd42929
zoe-woodward <zoewoodward94@gmail.com>
2024-10-24 18:40:06 +0100

    remove VIP lounge

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -28,21 +31,19 @@ app.use(
   })
 );
 
+app.use(passUserToView); 
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+
 
 app.use('/auth', authController);
+
+app.use(isSignedIn); // use new isSignedIn middleware here
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
