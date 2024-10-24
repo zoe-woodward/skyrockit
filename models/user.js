cbc3334835920d0dbde54e19f65a876dca93d178
zoe-woodward <zoewoodward94@gmail.com>
2024-10-24 18:42:00 +0100

    adding application schema to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+      company: {type: String, required: true},
+      title: {type: String, required: true},
+      notes: {type: String},
+      postingLink: {type: String},
+      status: {
+        type: {String},
+        enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+      }
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +20,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
